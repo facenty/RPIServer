fcc44f6bdef4b086b5f4b1dde1831572bb4fae5c
Oskar Chodowicz <ochodowicz@vewd.com>
2022-01-23 12:43:39 +0100

    Minor cleanup

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
-import threading
 import socketserver
-import os, sys
+import os
 import json
 
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         clientTypeJSON = str(self.request.recv(1024), "ascii")
-        print(clientTypeJSON)
+        # print(clientTypeJSON)
         clientTypeStr = json.loads(clientTypeJSON)["ClientType"]
         self.request.sendall(bytes("OK", "ASCII"))
         if clientTypeStr == "PUBLISHER":
@@ -33,21 +32,9 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
     HOST, PORT = "0.0.0.0", 9999
 
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     with server:
         ip, port = server.server_address
-
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        # server_thread = threading.Thread(target=server.serve_forever)
-        # Exit the server thread when the main thread terminates
-        # server_thread.daemon = True
-        # server_thread.start()
-        # server_thread.join()
         server.serve_forever()
-        # print("Server loop running in thread:", server_thread.name)
-
-        # server.shutdown()
